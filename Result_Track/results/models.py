57010554111eb582ca21e9d55876fc2c656b2471
Lachiecalder <lachiecalder@outlook.com>
2014-02-21 07:28:52 +1300

    Create results app skeleton and models

---
+++ Result_Track/results/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Student(models.Model):
+
+    # one to one relationship with a user authentication object
+    user = models.OneToOneField(User)
+
+    first_name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    teacher = models.ForeignKey('Teacher')
+
+class Teacher(models.Model):
+    title = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+
+class Result(models.Model):
+    student = models.ForeignKey('Student')
+    assignment = models.ForeignKey('Assignment')
+    mark = models.PositiveSmallIntegerField()
+
+class Assignment(models.Model):
+    due_date = models.DateField()
+    possible_mark = models.PositiveSmallIntegerField()
+    course = models.ForeignKey('Course')
+    name = models.CharField(max_length=50)
+    details = models.TextField
